5838155731147c1746a3d107a7801c628c58379a
aaalbinaaaa <albinarubanchuk@gmail.com>
2023-06-22 00:09:08 +0300

    add styles from card section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,7 @@ export class AppComponent implements OnInit{
   public currJson;
   public currencies: ICurrency;
   @Input() result: number = 0;
+  @Input() lastUpdated;
 
   @Input() currencyFrom: string = 'USD';
   @Input() currencyTo: string = 'USD';
@@ -29,16 +30,12 @@ export class AppComponent implements OnInit{
       this.parseJson(data);
       this.setCurrency(this.currJson.rates);
     });
-    console.log('changeBase amount value: ', this.amountValue);
-    console.log('changeBase result value: ', this.result);
-    // this.convert(this.amountValue);
   }
 
 
 
     toCountry(baseCode: string) {
       this.currencyTo = baseCode;
-      // this.convert(this.amountValue);
     }
 
   ngOnInit(): void {
@@ -53,39 +50,27 @@ export class AppComponent implements OnInit{
     this.currJson = JSON.stringify(data);
     this.currJson = JSON.parse(this.currJson);
     this.currencyFrom = this.currJson.base_code;
-    // console.log('base code ' + this.currencyFrom);
+    this.lastUpdated = this.currJson.time_last_update_utc;
   }
 
   setCurrency(data: ICurrency): void {
     this.currencies = data;
-    // console.log('rates ', this.currencies);
   } 
   
   convert(amount: number) {
-    console.log('convert amount: ', amount);
     for (let curr in this.currencies) {
       if (this.currencyTo === curr) {
         this.result = +(amount * this.currencies[curr]).toFixed(2);
       }
     }
-    console.log('convert result: ', this.result);
   }
 
   switchCurrencies() {
     let temp = this.currencyTo;
-    // this.currencyFrom = this.currencyTo;
-    // this.currencyTo = temp;
     this.amountValue = this.result;
     this.result = 0;
     this.changeBase(temp);
     this.toCountry(this.currencyFrom);
-    console.log('switchCurrencies from', this.currencyFrom);
-    console.log('switchCurrencies amount', this.amountValue);
-    console.log('switchCurrencies result', this.result);
-    console.log('switchCurrencies to', this.currencyTo);
-    // console.log('amount value', this.amountValue);
-    // this.convert(this.amountValue);
   }
 
-  
 }
